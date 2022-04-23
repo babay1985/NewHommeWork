40f83ab0205f1b492de8510a477e2887e7501699
Your Name <you@example.com>
2022-04-23 22:37:58 +0300

    что то новое нужно прописать сделать коммит

--- src/NewHome/Main.java
+++ src/NewHome/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello every one, i wright lesson and learning new profession");
         System.out.println("Everi one");
         System.out.println("Hello world 2");
+
     }
 }
