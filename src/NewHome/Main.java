04fb7e3d94a610832fd129744830d5aed1884b41
Your Name <you@example.com>
2022-04-23 22:33:08 +0300

    first commit может чтото не то

--- src/NewHome/Main.java
+++ src/NewHome/Main.java
@@ -3,7 +3,8 @@ package NewHome;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//
         System.out.println("Hello every one, i wright lesson and learning new profession");
+        System.out.println("Everi one");
     }
 }
